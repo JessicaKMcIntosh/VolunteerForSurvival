c30aaf8ca722716fe846a3963b2f4dabc337fe8b
Jessica K McIntosh <>
2024-10-01 16:47:03 -0700

    Started preparing for integration tests.

--- build.bat
+++ build.bat
@@ -26,7 +26,7 @@ REM Figure out what we were given on the command line.
     if /i "%1"=="build" CALL :runbuild
     if /i "%1"=="clean" CALL :runclean
     if /i "%1"=="debug" CALL :rundebug
-    if /i "%1"=="test"  CALL :runtests
+    if /i "%1"=="unit"  CALL :rununit
     if /i "%1"=="tests" CALL :runtests
     if /i "%1"=="help"  CALL :showhelp
     if /i "%1"=="/?"    CALL :showhelp
@@ -54,10 +54,10 @@ REM Build the game.
 REM Built the tests.
 :runbuildtest
     ECHO Building %APPNAME% tests...
-    IF EXIST "test_vts.z5" (
-        DEL test_vts.z5
+    IF EXIST "unit.z5" (
+        DEL unit.z5
     )
-    %COMPILER% +inform6 ++Extensions ++Source -S test_vts.inf
+    %COMPILER% +inform6 ++Extensions ++Source -S unit.inf
     EXIT /b
 
 REM Cleanup build artifacts.
@@ -66,8 +66,8 @@ REM Cleanup build artifacts.
     IF EXIST "vts.z5" (
         DEL vts.z5
     )
-    IF EXIST "test_vts.z5" (
-        DEL test_vts.z5
+    IF EXIST "unit.z5" (
+        DEL unit.z5
     )
     EXIT /b
 
@@ -80,13 +80,19 @@ REM Build the game with debug enabled.
     %COMPILER% +inform6 ++Extensions ++Source -SD vts.inf
     EXIT /b
 
-REM Run the tests.
-:runtests
+REM Run the unit tests.
+:rununit
     CALL :runbuildtest
     ECHO.
     ECHO Running %APPNAME% Unit tests...
-    ECHO. | %INTERPRETER% -h 100 -p -Z 2 test_vts.z5
+    ECHO. | %INTERPRETER% -h 100 -p -Z 2 unit.z5
     ECHO.
+    EXIT /b
+
+REM Run the tests.
+:runtests
+    ECHO Running %APPNAME% Unit tests...
+    CALL :rununit
     EXIT /b
 
 REM Show some help text.
@@ -100,7 +106,8 @@ REM Show some help text.
     ECHO clean - Delete all built files.
     ECHO debug - Build the game with debug enabled.
     ECHO help  - Display this help text.
-    ECHO test  - Build then run the %APPNAME% tests.
+    ECHO tests - Build and run all tests for %APPNAME%.
+    ECHO unit  - Build then run the %APPNAME% unit tests.
     GOTO exitscript
 
 REM Exit the script.
