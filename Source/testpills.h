decfe2532c1c3220b7b3bc986df7d2ad67087e7c
Jessica K McIntosh <>
2024-10-01 16:47:01 -0700

    Testing for taking pills.

--- Source/testpills.h
+++ Source/testpills.h
@@ -21,10 +21,12 @@ Include "pills";
 ! ------------------------------------------------------------------------------
 
 ! Messages for testing.
+Constant Pills_MSG_Eat_Empty "The pill bottle is empty.";
+Constant Pills_MSG_Eat_Full "You take a pill. You have 24 pills left.";
+Constant Pills_MSG_Eat_One "You take a pill. You have no more pills left.";
 Constant Pills_MSG_Number_Empty "Empty";
 Constant Pills_MSG_Number_One "1 pill";
 Constant Pills_MSG_Number_More "25 pills";
-
 
 ! ------------------------------------------------------------------------------
 ! Test Objects
@@ -32,13 +34,6 @@ Constant Pills_MSG_Number_More "25 pills";
 
 
 Pills_Class Pill_Bottle "pill bottle"
-;
-
-Object Pills_Not_Pills "Not Pills"
-;
-
-Object Pills_Food "Food"
-  has edible
 ;
 
 ! ------------------------------------------------------------------------------
@@ -65,11 +60,11 @@ Object Pills_Food "Food"
     Pill_Bottle,
     "The pill bottle was not created.");
 
-    assertOfClass(
-      Pill_Bottle,
-      Pills_Class,
-      "Pill bottle not created with the pills class."
-    );
+  assertOfClass(
+    Pill_Bottle,
+    Pills_Class,
+    "Pill bottle not created with the pills class."
+  );
 
   print "Success...^^";
 ];
@@ -77,12 +72,86 @@ Object Pills_Food "Food"
 [_TestPill_EatPill;
   print "Verify eating pills works corectly.^";
 
-  noun = Fake_Object;
-  ! Pills_EatSub();
+  ! Eat a pill from a full bottle.
+  noun = Pill_Bottle;
+  action = ##Eat;
+  Pill_Bottle.number = PILLS_DEFAULT_COUNT;
+  Pill_Bottle.time_left = 0;
+  WriteString(CheckString, Pills_MSG_Eat_Full);
+  CaptureOutput(_TestPill_HelperEatPill);
+
+  assertTrue(
+    (StrCmp(CheckString, PrintedString) == 0),
+    "Output message eating a pill is incorrect."
+  );
+
+  assertEquals(
+    (PILLS_DEFAULT_COUNT - 1),
+    Pill_Bottle.number,
+    "Pills should decrement after one is eaten."
+  );
+
+  assertEquals(
+    PILLS_DURATION,
+    Pill_Bottle.time_left,
+    "Pills should increment the amount of time left."
+  );
+
+  ! Eat a pill from a bottle with one pill.
+  noun = Pill_Bottle;
+  action = ##Eat;
+  Pill_Bottle.number = 1;
+  Pill_Bottle.time_left = 0;
+  WriteString(CheckString, Pills_MSG_Eat_One);
+  CaptureOutput(_TestPill_HelperEatPill);
+
+  assertTrue(
+    (StrCmp(CheckString, PrintedString) == 0),
+    "Output message eating a pill is incorrect, one pill."
+  );
+
+  assertEquals(
+    0,
+    Pill_Bottle.number,
+    "Pills should decrement after one is eaten, one pill."
+  );
+
+  assertEquals(
+    PILLS_DURATION,
+    Pill_Bottle.time_left,
+    "Pills should increment the amount of time left, one pill."
+  );
+
+  ! Eat a pill from an empty bottle.
+  noun = Pill_Bottle;
+  action = ##Eat;
+  Pill_Bottle.number = 0;
+  Pill_Bottle.time_left = 0;
+  WriteString(CheckString, Pills_MSG_Eat_Empty);
+  CaptureOutput(_TestPill_HelperEatPill);
+
+  assertTrue(
+    (StrCmp(CheckString, PrintedString) == 0),
+    "Output message eating a pill is incorrect."
+  );
+
+  assertEquals(
+    0,
+    Pill_Bottle.number,
+    "Pills should change after eating from an empty bottle."
+  );
+
+  assertEquals(
+    0,
+    Pill_Bottle.time_left,
+    "Time left should not change after eating from an empty bottle."
+  );
+
+  Pill_Bottle.number = PILLS_DEFAULT_COUNT;
+  Pill_Bottle.time_left = PILLS_DURATION;
 
   print "Success...^^";
 ];
-
 
 [_TestPill_PrintNumber;
   print "Verify the number of pills is printed corectly.^";
@@ -124,4 +193,8 @@ Object Pills_Food "Food"
 
 [_TestPill_HelperPrintNumber;
   Pills_Print_Number(Pill_Bottle);
+];
+
+[_TestPill_HelperEatPill;
+  Pill_Bottle.before();
 ];
