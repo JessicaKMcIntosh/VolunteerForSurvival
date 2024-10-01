72cbb8bbd02c7ec4c7d78c6ff64e105b29508f36
Jessica K McIntosh <>
2024-10-01 16:47:02 -0700

    More help text added.

--- Source/help.h
+++ Source/help.h
@@ -36,12 +36,14 @@ Constant Help_Text_Notword "That is not a valid word in this game.^";
 Constant Help_Text_Unknown "Help is not available on this topic.^";
 Constant Help_Text_Words
  "These are the words used in this game:^
-  EAT EXAMINE INVENTORY OPEN LOOK PLAY TAKE USE^";
+  EAT EXAMINE GO INVENTORY OPEN LOOK PLAY SCORE TAKE TURN USE^
+  Directions: NORTH SOUTH EAST WEST UP DOWN^";
 
 ! Help text for specific words.
 Constant Help_Text_Word_Close "CLOSE a container.";
 Constant Help_Text_Word_Eat "Use 'EAT PILL' to consume a pill from the bottle.^";
-Constant Help_Text_Word_Examine "Give detailed information on any object or direction.^";
+Constant Help_Text_Word_Examine "EXAMINE an object or direction to get detailed information.^";
+Constant Help_Text_Word_Go "GO in a specific direction. NORTH, SOUTH, EAST, WEST, UP, DOWN^";
 Constant Help_Text_Word_Inventory "INVENTORY shows what the player is currently carrying.^";
 Constant Help_Text_Word_Open "OPEN a container to interact with the contents.^";
 Constant Help_Text_Word_Look
@@ -53,6 +55,8 @@ Constant Help_Text_Word_Play
 Constant Help_Text_Word_Take
  "TAKE takes an object from the ground.^
   TAKE ALL takes all available objects on ground or from open containers.^";
+Constant Help_Text_Word_Turn "TURN an object on or off.^";
+Constant Help_Text_Word_Score "SCORE shows your current score out of the maximum score.^";
 Constant Help_Text_Word_Use "USE attempts to do the right thing for an item. Try it.^";
 
 ! ------------------------------------------------------------------------------
@@ -74,18 +78,6 @@ Global Help_Topic_Word = nothing;
   Help_Topic = true;
 ];
 
-! Print general help for playing the game.
-[HelpGeneral;
-  print
-   "Welcome to ", (string) Story, "!^
-    This is a text adventure game along the lines of Adventure and Zork.^
-    To interact with the game use one command per line.^
-    Examples are LOOK, EXAMINE, OPEN, TAKE and so on.^
-    ^
-    To get help on a topic or object type 'HELP TOPIC'.^
-    To get a list of words used in the game type 'HELP WORDS'.^";
-];
-
 ! Print help text for the user.
 [HelpSub;
   ! Do we have a noun?
@@ -97,11 +89,14 @@ Global Help_Topic_Word = nothing;
         'close':    print (string) Help_Text_Word_Close;
         'eat':      print (string) Help_Text_Word_Eat;
         'examine':  print (string) Help_Text_Word_Examine;
+        'go':       print (string) Help_Text_Word_Go;
         'inventory':print (string) Help_Text_Word_Inventory;
         'look':     print (string) Help_Text_Word_Look;
         'open':     print (string) Help_Text_Word_Open;
         'play':     print (string) Help_Text_Word_Play;
         'take':     print (string) Help_Text_Word_Take;
+        'turn':     print (string) Help_Text_Word_Turn;
+        'score':    print (string) Help_Text_Word_Score;
         'use':      print (string) Help_Text_Word_Use;
         'words':    print (string) Help_Text_Words;
         default:    print (string) Help_Text_Unknown;
