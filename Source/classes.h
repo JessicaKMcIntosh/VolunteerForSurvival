5ec61e9800dacac5aee88dd93083df297a6c921a
Jessica K McIntosh <>
2024-10-11 18:09:08 -0700

    Moved the world classes to a separate file.

---
+++ Source/classes.h
@@ -0,0 +1,61 @@
+! ------------------------------------------------------------------------------
+! classes - All of the classes used by the world that are not part of a libary.
+!
+! Created by Jessica McIntosh
+! JessicaKMcIntosh AT gmail DOT com
+!
+! This work is licensed under the Creative Commons Attribution-ShareAlike 4.0
+! International License. See the file LICENSE for details.
+! ------------------------------------------------------------------------------
+! USAGE
+! ------------------------------------------------------------------------------
+! Include after Grammar but before loading world areas.
+! ------------------------------------------------------------------------------
+
+! ------------------------------------------------------------------------------
+! Meta Data
+! ------------------------------------------------------------------------------
+
+System_file;
+
+Message "Loading the Classes library.";
+
+! ------------------------------------------------------------------------------
+! Classes
+! ------------------------------------------------------------------------------
+
+! Just a basic room.
+Class Room_Class
+  has light
+;
+
+! A dark room.
+Class Dark_Room_Class
+;
+
+! City streets.
+Class City_Class
+  has light
+;
+
+! Subways.
+Class Subway_Class
+;
+
+! Storm drains.
+Class Storm_Class
+;
+
+! Object the player can posess.
+Class Item_Class
+;
+
+! Scenery.
+Class Scenery_Class
+  has static
+;
+
+! A door.
+Class Door_Class
+  has static door openable
+;
